0f2ed9c0a7c5c51ff5bafa3c2540d988e2eeb8c5
razha123 <razha9046123@gmail.com>
2022-04-27 07:28:22 -0700

    upload

---
+++ engine.js
@@ -0,0 +1,9 @@
+function open1() {
+    document.getElementById("sideplan").style.width = "210px";
+    document.getElementById("sideplan").style.boxShadow = "4px 4px 10px 1000px rgba(50, 50, 50, 0.75)";
+}
+
+function close1() {
+    document.getElementById("sideplan").style.width = "0";
+    document.getElementById("sideplan").style.boxShadow = "0 0 0 0 rgba(50, 50, 50, 0.75)";
+}
